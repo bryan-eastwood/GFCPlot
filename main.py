36a25f157a687e3bcb79f95e8f5b1f070b8f39c3
Bryan <bryane@protonmail.ch>
2017-08-24 17:08:01 +0000

    Dev changes

--- main.py
+++ main.py
@@ -65,6 +65,7 @@ for row in data:
 
 print(" DONE")
 
+plt.ion()
 fig, ax = plt.subplots();
 
 ax.set_ylabel('Temperature (\N{DEGREE SIGN}F)')
@@ -81,18 +82,24 @@ if PROMPT_FOR_BOUNDS:
 
 	limit_range(lower, upper)
 
-lines = []
-for name, data in sorted(devices.items(), key=operator.itemgetter(0)):
-	lines.append(plot_coords(ax, data, name))
-
-leg = ax.legend(loc='upper center', bbox_to_anchor=(0.5, 1.05),
-      		ncol=3, fancybox=True, shadow=True)
-
-leg.get_frame().set_alpha(0.4)
 lined = dict()
-for legline, origline in zip(leg.get_lines(), lines):
-    legline.set_picker(5)  # 5 pts tolerance
-    lined[legline] = origline
+def plot_routine():
+	global lined
+	ax.lines = []
+	lines = []
+	for name, data in sorted(devices.items(), key=operator.itemgetter(0)):
+		lines.append(plot_coords(ax, data, name))
+
+	leg = ax.legend(loc='upper center', bbox_to_anchor=(0.5, 1.05),
+      	         	ncol=3, fancybox=True, shadow=True)
+
+	lined = dict()
+	leg.get_frame().set_alpha(0.4)
+	for legline, origline in zip(leg.get_lines(), lines):
+	    legline.set_picker(5)  # 5 pts tolerance
+	    lined[legline] = origline
+
+plot_routine()
 
 
 def onpick(event):
@@ -106,27 +113,29 @@ def onpick(event):
         legline.set_alpha(0.2)
     fig.canvas.draw()
 
-fig.canvas.mpl_connect('pick_event', onpick)
+#def update_temp():
+while(True):
+	print("Querying...")
+	data = csv.reader(codecs.iterdecode(urllib.request.urlopen("http://10.199.251.176:44449/GFC_data.csv"), 'utf-8'), delimiter=",")
+	for row in data:
+		date = get_epoch(row[0])
+		print("%d > %d" % (date, most_recent))
+		if(date > most_recent):
+			add_row(row[1].strip(" \""), date, row[2]);
+	print(len(devices.items()))
+	print("Done")
+	fig.canvas.mpl_connect('pick_event', onpick)
 
-def update_temp():
-	while(True):
-		print("Querying...")
-		data = csv.reader(codecs.iterdecode(urllib.request.urlopen("http://10.199.251.176:44449/GFC_data.csv"), 'utf-8'), delimiter=",")
-		for row in data:
-			date = get_epoch(row[0])
-			print("%d > %d" % (date, most_recent))
-			if(date > most_recent):
-				add_row(row[1].strip(" \""), date, row[2]);
-		print("Done")
-
-		current_temps = []
-		for name, data in sorted(devices.items(), key=operator.itemgetter(0)):
-			current_temps.append([name, float(data[-1][1])])
-		label = ", ".join('%s: %.2f' % (x[0], x[1]) for x in current_temps)
-		index = label.find(',', int(len(label)/2)) + 2
-		ax.set_xlabel(label[:index] + '\n' + label[index:])
-		fig.canvas.draw()
-		time.sleep(1)
+	current_temps = []
+	for name, data in sorted(devices.items(), key=operator.itemgetter(0)):
+		current_temps.append([name, float(data[-1][1])])
+	label = ", ".join('%s: %.2f' % (x[0], x[1]) for x in current_temps)
+	index = label.find(',', int(len(label)/2)) + 2
+	ax.set_xlabel(label[:index] + '\n' + label[index:])
+	#plot_routine()
+	fig.canvas.draw()
+	#plt.draw()
+	time.sleep(1)
 
 thread = Thread(target = update_temp)
 
